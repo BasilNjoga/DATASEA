96d3832f1a232db757d359f3bd028bba843b548a
Basil Njoga <Basilnjoga@gmail.com>
2022-12-21 13:18:31 +0300

    check

--- flasktest.py
+++ flasktest.py
@@ -1,4 +1,4 @@
-#!/bin/python3
+#!/usr/bin/python3
 
 from flask import Flask, render_template
 import requests
@@ -6,14 +6,21 @@ import json
 
 app = Flask(__name__)
 
-def get_chart():
-    url = "https://quickchart.io/chart?c={type:'bar',data:{labels:['Q1','Q2','Q3','Q4'], datasets:[{label:'Users',data:[50,60,70,180]},{label:'Revenue',data:[100,200,300,400]}]}}"
-
-    r = json.loads(requests.request("GET", url).text)
-    
-
 @app.route("/")
 def index():
     return render_template("index.html")
 
-app.run(host="0.0.0.0", port=80)
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
+
+@app.route("/visualise")
+def visualisation():
+    return render_template("visualisation.html")
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=80)
